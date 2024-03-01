2dfa977be7b0458942ace6b1ab141c695eeeaea5
Wafaamohamed2 <121331529+Wafaamohamed2@users.noreply.github.com>
2024-03-01 15:26:07 +0200

    Add files via upload
    Bubble sort in C#

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of elements in the array: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[] array = new int[n];
+
+            Console.WriteLine("Enter the elements:");
+
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            Bubblesort(array, n);
+         
+        }
+
+        static void Bubblesort( int[] array,int n)
+        {
+            // Sort the array using Bubble Sort
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - 1 - i; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        // Swap elements if they are in the wrong order
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+            }
+
+            // Print the sorted array
+            Console.Write("Sorted array is :");
+
+            foreach (int element in array)
+            {
+                Console.Write(element + " ");
+            }
+        }
+    }
+}
